6a291b912501046db3fbf130958d1fe6d2111caa
Muhwezi Allan <Muhwezi Allan>
2017-04-19 15:20:39 +0300

    initial commit of Day3 assignment - wordcount

---
+++ Day3/WordCount/wordcount.py
@@ -0,0 +1,14 @@
+def wordcount(words):
+    if not isinstance(words, str):
+        raise TypeError("not a string type")
+
+    word_counter = dict()
+
+    list_words = words.strip(" \t\n\r\v\f").split()
+    for word in list_words:
+        if word not in word_counter:
+            word_counter[word] = 1
+        else:
+            word_counter[word] += 1
+    return word_counter
+    
